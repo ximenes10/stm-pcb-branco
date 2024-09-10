2b5db7d898ddcabe5dd1077b0c2c3452edd32308
Ximenes <ximenes10@gmail.com>
2024-09-09 22:34:03 -0300

    Inclusão da biblioteca math.h

--- TESTE_Ver_1/Core/Src/main.c
+++ TESTE_Ver_1/Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "math.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
